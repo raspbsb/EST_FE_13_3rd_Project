2c61b5d7aa4e2fbd4ba6cc81f38b7d8ebd647acc
Raspbsb <unionbjh@naver.com>
2026-08-03 19:08:21 +0900

    refactor: supabase.ts를 services로 분리

--- app/src/services/supabase.ts
+++ app/src/services/supabase.ts
@@ -4,7 +4,4 @@ import type { Database } from "../types/database.types";
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 
-export const supabase = createClient<Database>(
-  supabaseUrl,
-  supabaseKey,
-);
+export const supabase = createClient<Database>(supabaseUrl, supabaseKey);
