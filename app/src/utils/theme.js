a8425d013b1dc506e46c44ee5d7f2133ec1a1fd3
rozer4heros <rozer4heros@gmail.com>
2026-08-05 15:09:27 +0900

    feat: MUI 사용자 지정 테마 적용

---
+++ app/src/utils/theme.js
@@ -0,0 +1,21 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    background: {
+      default: "#FBFCFD",
+    },
+    primary: {
+      main: "#0D6EFD",
+      light: "#569AFE",
+    },
+    secondary: {
+      main: "#EDEDED",
+    },
+    text: {
+      primary: "#212121",
+    },
+  },
+});
+
+export default theme;
