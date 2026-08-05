ccd3a401345fefe081f500fcc73fa92c8210bee1
rozer4heros <rozer4heros@gmail.com>
2026-08-05 17:01:55 +0900

    refactor: Portfolio 페이지 컴포넌트 import 방식 변경

--- app/src/components/Portfolio/index.js
+++ app/src/components/Portfolio/index.js
@@ -1,6 +1,4 @@
-import HeroSection from "./HeroSection";
-import DescriptionSection from "./DescriptionSection";
-import AuthorInfoSection from "./AuthorInfoSection";
-import AiSummarySection from "./AiSummarySection";
-
-export { HeroSection, DescriptionSection, AuthorInfoSection, AiSummarySection };
+export { default as HeroSection } from "./HeroSection";
+export { default as DescriptionSection } from "./DescriptionSection";
+export { default as AuthorInfoSection } from "./AuthorInfoSection";
+export { default as AiSummarySection } from "./AiSummarySection";
--- app/src/pages/Portfolio.jsx
+++ app/src/pages/Portfolio.jsx
@@ -1,6 +1,6 @@
 import Text from "@mui/material/Typography";
 
-import * as Sections from "../components/Portfolio/index";
+import { HeroSection, DescriptionSection, AiSummarySection, AuthorInfoSection } from "../components/Portfolio";
 
 export default function Portfolio() {
   return (
@@ -8,10 +8,10 @@ export default function Portfolio() {
       <Text component={"p"} variant="h4">
         포트폴리오 상세
       </Text>
-      <Sections.HeroSection />
-      <Sections.DescriptionSection />
-      <Sections.AiSummarySection />
-      <Sections.AuthorInfoSection />
+      <HeroSection />
+      <DescriptionSection />
+      <AiSummarySection />
+      <AuthorInfoSection />
     </>
   );
 }
