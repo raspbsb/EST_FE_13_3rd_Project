9cf6ea77e539b8cc39e94c916bc5c0321440ea21
rozer4heros <rozer4heros@gmail.com>
2026-08-06 04:48:53 +0900

    feat: MUI 반응형 Breakpoint 설정
    * mobile: 0
    * tablet: 768
    * desktop: 1440

--- app/src/styles/theme.js
+++ app/src/styles/theme.js
@@ -1,6 +1,13 @@
 import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
+  breakpoints: {
+    values: {
+      mobile: 0,
+      tablet: 768,
+      desktop: 1440,
+    },
+  },
   palette: {
     background: {
       default: "#FBFCFD",
