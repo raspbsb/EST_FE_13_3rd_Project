379bded554d46f36bf7cc3114d67e5b820510e6c
rozer4heros <rozer4heros@gmail.com>
2026-08-05 17:03:30 +0900

    refactor: components\icons\index.js 아이콘 임포트 배럴 파일 생성

--- app/src/components/Portfolio/DescriptionSection.jsx
+++ app/src/components/Portfolio/DescriptionSection.jsx
@@ -4,17 +4,6 @@ import Box from "@mui/material/Box";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 
-import EditIcon from "@mui/icons-material/Edit";
-import ViewsIcon from "@mui/icons-material/VisibilityOutlined";
-import LikeIcon from "@mui/icons-material/FavoriteBorder";
-import LikeIconActive from "@mui/icons-material/Favorite";
-import StarIcon from "@mui/icons-material/StarBorder";
-import StarIconActive from "@mui/icons-material/Star";
-import LinkIcon from "@mui/icons-material/Link";
-import CodeIcon from "@mui/icons-material/Code";
-import AiIcon from "@mui/icons-material/AutoAwesome";
-import EmailIcon from "@mui/icons-material/EmailOutlined";
-
 export default function DescriptionSection({}) {
   return (
     <Box component={"section"}>
