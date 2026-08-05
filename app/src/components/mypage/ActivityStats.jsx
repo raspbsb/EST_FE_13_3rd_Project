063503d5ea1494b18043efb8d153a10e2d3bcd80
rkskek8484-cell <rkskek8484@gmail.com>
2026-08-06 03:01:02 +0900

    style: Update ActivityStats component layout using MUI Grid for improved UI

--- app/src/components/mypage/ActivityStats.jsx
+++ app/src/components/mypage/ActivityStats.jsx
@@ -1,3 +1,12 @@
+import theme from '../../styles/theme';
+
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActionArea from '@mui/material/CardActionArea';
+import CardContent from '@mui/material/CardContent';
+import Text from '@mui/material/Typography';
+
 export default function ActivityStats() {
   const stats = [
     {
@@ -18,14 +27,27 @@ export default function ActivityStats() {
     },
   ];
   return (
-    <section>
-      {stats.map(stat => (
-        <div className='' key={stat.title}>
-          <h3>{stat.title}</h3>
-
-          <p>{stat.count}</p>
-        </div>
+    <Grid container spacing={3} sx={{ justifyContent: 'center', bgcolor: '#000', borderRadius: 3, py: 3 }}>
+      {stats.map(item => (
+        <Grid key={item.idx}>
+          <Text variant='h6'>{item.title}</Text>
+          <Box sx={{ display: 'flex' }}>
+            <Text variant='h4'>{item.count}</Text>
+            <Text variant='h5'>개</Text>
+          </Box>
+        </Grid>
       ))}
-    </section>
+    </Grid>
   );
 }
+// <section>
+//   {stats.map(stat => (
+//     <div className='' key={stat.title}>
+//       <h3>{stat.title}</h3>
+
+//       <p>{stat.count}</p>
+//     </div>
+//   ))}
+// </section>
+//   );
+// }
