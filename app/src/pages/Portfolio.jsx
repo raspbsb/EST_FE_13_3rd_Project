db292abef92bd0917575f2a28bd26f454bb53d8a
rozer4heros <rozer4heros@gmail.com>
2026-08-10 06:00:27 +0900

    feat: 기초 예외 처리 (통신 오류, 없는 포트폴리오)

--- app/src/pages/Portfolio.jsx
+++ app/src/pages/Portfolio.jsx
@@ -1,11 +1,12 @@
 import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { supabase } from "../utils/supabase";
 
 import Text from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import Stack from "@mui/material/Stack";
+import MuiLink from "@mui/material/Link";
 
 import { setLoading, setPortfolio, resetPortfolio } from "../components/Portfolio/portfolioSlice";
 import { HeroSection, DescriptionSection, AiSummarySection, AuthorInfoSection } from "../components/Portfolio";
@@ -28,8 +29,59 @@ export default function Portfolio() {
     };
   }, [id]);
 
+  if (status === "failed") {
+    return (
+      <Container>
+        <Text component={"p"} variant="h4">
+          포트폴리오 상세
+        </Text>
+        <Text component={"h1"} variant="h3" sx={{ my: 6 }}>
+          DB와 통신에 실패했습니다.
+        </Text>
+        {/*
+        <Text component={"p"} variant="h5">
+          {error}
+        </Text>
+        */}
+        <Text component={"p"} variant="body1">
+          <MuiLink component={Link} to={"/"}>
+            홈으로 돌아가기
+          </MuiLink>
+        </Text>
+        <Text component={"p"} variant="body1">
+          <MuiLink component={Link} to={"/gallery"}>
+            목록으로 돌아가기
+          </MuiLink>
+        </Text>
+      </Container>
+    );
+  }
+  /*
+  if (status === "notFound") {
+    return (
+      <Container>
+        <Text component={"p"} variant="h4">
+          포트폴리오 상세
+        </Text>
+        <Text component={"h1"} variant="h3" sx={{ my: 6 }}>
+          해당하는 포트폴리오가 없습니다.
+        </Text>
+        <Text component={"p"} variant="body1">
+          <MuiLink component={Link} to={"/"}>
+            홈으로 돌아가기
+          </MuiLink>
+        </Text>
+        <Text component={"p"} variant="body1">
+          <MuiLink component={Link} to={"/gallery"}>
+            목록으로 돌아가기
+          </MuiLink>
+        </Text>
+      </Container>
+    );
+  }
+  */
   return (
-    <Container maxWidth={"desktopContainer"}>
+    <Container>
       <Stack sx={{ gap: { mobile: 3, tablet: 4, desktop: 6 }, py: { mobile: 4, tablet: 4, desktop: 6 } }}>
         <Text component={"p"} variant="h4">
           포트폴리오 상세
